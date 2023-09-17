c639596d3a8b31a2047d05f3be92dd51cb967732
Im-Rises <quentin-morel88@hotmail.com>
2023-09-16 20:06:49 -0400

    New version

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,96 @@
 import {useState} from 'react'
-import './App.css'
+import './App.scss'
 import {NbodySimulatorBarnesHut, Color} from "nbody-simulator-react-barnes-hut-react-p5";
 
 function App() {
-    const [count, setCount] = useState(1000)
+    const [count, setCount] = useState(1000);
+    const [maxDepth, setMaxDepth] = useState(1000);
+    const [theta, setTheta] = useState(1);
+    const [G, setG] = useState(1);
+    const [softening, setSoftening] = useState(100);
+    const [capacity, setCapacity] = useState(1);
+    const [particleMass, setParticleMass] = useState(1);
 
     return (
         <>
-            <NbodySimulatorBarnesHut particlesCount={count} widthHeight={500} showQuadtree={false} maxDepth={1000}
-                                     theta={1} G={1} softening={100} capacity={1} particleMass={1}
+            <h1>Barnes-Hut N-Body simulation</h1>
+            <NbodySimulatorBarnesHut particlesCount={count} widthHeight={500} showQuadtree={false} maxDepth={maxDepth}
+                                     theta={theta} G={G} softening={softening} capacity={capacity}
+                                     particleMass={particleMass}
                                      backgroundColor={new Color(100, 100, 100)}/>
-            <input type={'number'} value={count} min={1} onChange={(e) => {
-                if (e.target.value === '' || parseInt(e.target.value) < 1) {
-                    setCount(parseInt("1"));
-                    return;
-                }
-                setCount(parseInt(e.target.value))
-            }
-            }/>
+            <div className={'input-simulation-holder'}>
+                <div>
+                    <label htmlFor={'input-particles-count'}>Particles count : </label>
+                    <input type={'number'} name={'input-particles-count'} value={count} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setCount(parseInt("1"));
+                            return;
+                        }
+                        setCount(parseInt(e.target.value))
+                    }
+                    }/>
+                </div>
+                <div>
+                    <label htmlFor={'input-max-depth'}>Max depth : </label>
+                    <input type={'number'} name={'input-max-depth'} value={maxDepth} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setMaxDepth(parseInt("1"));
+                            return;
+                        }
+                        setMaxDepth(parseInt(e.target.value))
+                    }}/>
+                </div>
+                <div>
+                    <label htmlFor={'input-theta'}>Theta : </label>
+                    <input type={'number'} name={'input-theta'} value={theta} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setTheta(parseInt("1"));
+                            return;
+                        }
+                        setTheta(parseInt(e.target.value))
+                    }}/>
+                </div>
+                <div>
+                    <label htmlFor={'input-g'}>G : </label>
+                    <input type={'number'} name={'input-g'} value={G} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setG(parseInt("1"));
+                            return;
+                        }
+                        setG(parseInt(e.target.value))
+                    }}/>
+                </div>
+                <div>
+                    <label htmlFor={'input-softening'}>Softening : </label>
+                    <input type={'number'} name={'input-softening'} value={softening} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setSoftening(parseInt("1"));
+                            return;
+                        }
+                        setSoftening(parseInt(e.target.value))
+                    }}/>
+                </div>
+                <div>
+                    <label htmlFor={'input-capacity'}>Capacity : </label>
+                    <input type={'number'} name={'input-capacity'} value={capacity} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setCapacity(parseInt("1"));
+                            return;
+                        }
+                        setCapacity(parseInt(e.target.value))
+                    }}/>
+                </div>
+                <div>
+                    <label htmlFor={'input-particle-mass'}>Particle mass : </label>
+                    <input type={'number'} name={'input-particle-mass'} value={particleMass} min={1} onChange={(e) => {
+                        if (e.target.value === '' || parseInt(e.target.value) < 1) {
+                            setParticleMass(parseInt("1"));
+                            return;
+                        }
+                        setParticleMass(parseInt(e.target.value))
+                    }}/>
+                </div>
+            </div>
         </>
     )
 }
